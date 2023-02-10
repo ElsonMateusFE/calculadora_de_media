80c3731cf9451b187a166a07d991966fc6754394
Elson Mateus <111660816+ElsonMateusFE@users.noreply.github.com>
2023-02-10 18:45:23 -0300

    atualizacao

--- revisao.js
+++ revisao.js
@@ -1,47 +1,43 @@
 const form = document.querySelector('#form-atividade')
-const nomeAtividade = document.querySelector('#nome-atividade')
-const notaAtividade = document.querySelector('#nota-atividade')
-const imgProvado = '<img src="./images/aprovado.png" alt="Emoji festejando">'
-const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepção>'
-const spanArovado = '<span class="resultado aprovado">Aprovado</span>'
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji festejando">'
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepção">'
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>'
 const spanReprovado = '<span class="resultado reprovado">Reprovado</span>'
 const atividades = []
 const notas = []
-const escolheMedia = Number(prompt('Escolha uma média:'))
 let linhas = ''
 
 form.addEventListener('submit', function(e){
     e.preventDefault()
 
-    addAtividadeNotas()
-    inputAtividadeNotas()
+    recebendoDados()
+    calculandoMedia()
     mediaFinal()
 })
 
-function addAtividadeNotas(){
+function recebendoDados(){
+    const nomeAtividade = document.getElementById('nome-atividade')
+    const notaAtividade = document.getElementById('nota-atividade')
 
     if(atividades.includes(nomeAtividade.value)){
-        alert(`${nomeAtividade.value} está em uso`)
+        alert(`A atividade ${nomeAtividade.value} já está em uso`)
     }else{
         atividades.push(nomeAtividade.value)
         notas.push(Number(notaAtividade.value))
     
         let linha = '<tr>'
         linha += `<td>${nomeAtividade.value}</td>`
-        linha += `<td>${notaAtividade.value}`
-        linha += `<td>${notaAtividade.value >= escolheMedia ? imgProvado : imgReprovado}</td>`
+        linha += `<td>${notaAtividade.value}</td>`
+        linha += `<td>${notaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`
         linha += '</tr>'
-        
+    
         linhas += linha
-    } 
+    
+        document.querySelector('tbody').innerHTML = linhas
+    }
 }
 
-function inputAtividadeNotas(){
-    const tBody = document.querySelector('tbody')
-    tBody.innerHTML = linhas
-}
-
-function somaDasNotasEMedias(){
+function calculandoMedia(){
     let soma = 0
 
     for(let i = 0; i < notas.length; i++){
@@ -52,8 +48,8 @@ function somaDasNotasEMedias(){
 }
 
 function mediaFinal(){
-    const mediaDoAluno = somaDasNotasEMedias()
+    const media = calculandoMedia()
 
-    document.querySelector('#media-final-valor').innerHTML = mediaDoAluno.toFixed(2)
-    document.querySelector('#media-final-resultado').innerHTML = mediaDoAluno >= escolheMedia ? spanArovado : spanReprovado
+    document.getElementById('media-final-valor').innerHTML = media.toFixed(2)
+    document.getElementById('media-final-resultado').innerHTML = media >= 7 ? spanAprovado : spanReprovado
 }
